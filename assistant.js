7968e22220d5066ac751598f7312e05117aaf210
Vignesh Kalyanasundaram <vignesh20092001@gmail.com>
2025-07-12 04:34:32 -0700

    3 methods addtask(), completetask(), reportmood()

--- assistant.js
+++ assistant.js
@@ -5,3 +5,22 @@ function PersonalAssistant(name) {
   this.mood = "neutral";
 }
 
+PersonalAssistant.prototype.addTask = function(task) {
+  this.tasks.push(task);
+  console.log(`Task added: ${task}`);
+};
+
+PersonalAssistant.prototype.completeTask = function() {
+  if (this.tasks.length > 0) {
+    const done = this.tasks.shift();
+    console.log(`Completing task: ${done}`);
+    this.mood = "accomplished";
+  } else {
+    console.log("No tasks left to complete!");
+  }
+};
+
+PersonalAssistant.prototype.reportMood = function() {
+  console.log(`Mood: ${this.mood}`);
+};
+
